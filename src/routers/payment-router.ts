28a12aedd7dd9b1768b1d228d70a740b4d855627
TamamTamar <tamar.tamam770@gmail.com>
2025-04-23 08:11:40 +0300

    w

--- src/routers/payment-router.ts
+++ src/routers/payment-router.ts
@@ -9,8 +9,8 @@ const allowedIP = "18.194.219.73";
 
 router.post("/nedarim", async (req, res) => {
     try {
-        // בדיקת כתובת ה-IP של הבקשה
         const requestIP = req.headers["x-forwarded-for"] || req.connection.remoteAddress;
+        console.log(`Request received from IP: ${requestIP}`);
         if (requestIP !== allowedIP) {
             console.error(`Unauthorized access attempt from IP: ${requestIP}`);
             return res.status(403).send("Forbidden: Unauthorized IP");
@@ -18,34 +18,32 @@ router.post("/nedarim", async (req, res) => {
 
         const data = req.body;
 
-        // יצירת אובייקט חדש עם הנתונים
         const newPaymentData = {
-            Mosad: "7013920",
+            Mosad: data.MosadNumber,
             ApiValid: "zidFYCLaNi",
             Zeout: data.Zeout,
-            FirstName: data.FirstName,
-            LastName: data.LastName,
-            Street: data.Street,
-            City: data.City,
+            FirstName: data.ClientName.split(" ")[0],
+            LastName: data.ClientName.split(" ")[1] || "",
+            Street: data.Adresse,
+            City: "",
             Phone: data.Phone,
             Mail: data.Mail,
-            PaymentType: data.Is12Months ? "HK" : "Ragil",
-            Amount: data.annualAmount,
-            Tashlumim: data.Is12Months ? 12 : data.Tashlumim,
-            Currency: 1,
+            PaymentType: data.TransactionType,
+            Amount: parseFloat(data.Amount),
+            Tashlumim: parseInt(data.Tashloumim),
+            Currency: parseInt(data.Currency),
             Groupe: data.Groupe,
-            Comment: data.Comment,
+            Comment: data.Comments,
             CallBack: "https://node-beit-chabad-yaffo.onrender.com/api/payment/nedarim",
             CallBackMailError: "lchabadyaffo@gmail.com",
         };
 
-        // שמירת הנתונים ב-Database
         const payment = new Payment(newPaymentData);
         await payment.save();
 
         console.log("Payment data saved successfully:", payment);
 
-        res.status(200).send("OK"); // להחזיר 200 כדי שהמערכת שלהם לא תשלח שגיאה
+        res.status(200).send("OK");
     } catch (error) {
         console.error("Error handling callback:", error);
         res.status(500).send("Internal Server Error");
