0d2b2e3fb636cef1d4214a58ee2897d50c478ce4
TamamTamar <tamar.tamam770@gmail.com>
2025-06-05 19:45:50 +0300

    b

--- src/db/init-db.ts
+++ src/db/init-db.ts
@@ -2,23 +2,15 @@ import { Logger } from "../logs/logger";
 import { donations } from "./initial-data";
 import { Payment } from "./models/PaymentModel";
 
-
 const initDB = async () => {
   try {
-    const donationsCount = await Payment.countDocuments();
-
-    if (donationsCount >= 1000000) {
-      Logger.log("1 or more donations already exist. Skipping donation initialization.");
-    } else {
-      for (let d of donations) {
-        const saved = await Payment.create(d);
-        Logger.verbose(`Donation created: ${saved.Phone}`);
-      }
+    for (let d of donations) {
+      const saved = await Payment.create(d);
+      Logger.verbose(`Donation created: ${saved.Phone}`);
     }
   } catch (e) {
     Logger.error(`Database initialization failed: ${e.message}`);
   }
 };
-
 
 export default initDB;
